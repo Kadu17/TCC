6e578e91045d74d1fa26688414858ed1734a4767
Kadu17 <carloskduk10@gmail.com>
2024-05-06 16:49:07 -0300

    Home

--- src/assets/Pages/Cadastro/Home.jsx
+++ src/assets/Pages/Cadastro/Home.jsx
@@ -12,15 +12,15 @@ import { GoLightBulb } from "react-icons/go";
 function Home() {  //TELA INICIAL DA PLATAFORMA 
     return (
         <div>
-            <div className={'flex items-center'}>
-                <div className={''}>
+            <div className={'flex items-center justify-center'}>
+                <div className={'sm:w-11/12 2xl:w-6/12'}>
                     <h1 className={'p-4 mb-2 ml-10 text-4xl font-semibold bg-gradient-to-r from-gradiente2 to-gradiente1 inline-block text-transparent bg-clip-text'}>Plataforma online de Feedback</h1>
-                    <p className={'ml-14 text-base'}>O Meinung tem por objetivo melhorar o processo do feedback
+                    <p className={'ml-14 text-base 2xl:text-xl'}>O Meinung tem por objetivo melhorar o processo do feedback
                         entre os aprendizes e instrutores.</p>
-                    <p className={'ml-14 text-base'}>Dessa forma, os dados ficam centralizados
+                    <p className={'ml-14 text-base 2xl:text-xl'}>Dessa forma, os dados ficam centralizados
                         contribuindo para o funcionamento de maneira mais rápida e fácil para todos os envolvidos.</p>
                 </div>
-                <div className={'flex justify-end mr-10 md:p-8 '}>
+                <div className={'flex justify-end  mr-10 sm:w-96 sm:mt-14 sm:mr-2 md:p-8 '}>
                     <img src={animação1} alt="" />
                 </div>
             </div>
@@ -32,7 +32,7 @@ function Home() {  //TELA INICIAL DA PLATAFORMA
                             <p className={'text-white mt-2'}>IDEIA</p>
                         </div>
 
-                        <p className={'text-base'}>A ideia para o projeto surgiu em uma reunião com o Henrique Dona, nosso gestor. Além disso, também percebemos que alguns processos do feedback poderiam ser automatizados para ser mais rápido.</p>
+                        <p className={'text-base 2xl:text-xl'}>A ideia para o projeto surgiu em uma reunião com o Henrique Dona, nosso gestor. Além disso, também percebemos que alguns processos do feedback poderiam ser automatizados para ser mais rápido.</p>
                     </div>
                 </div>
 
@@ -43,19 +43,19 @@ function Home() {  //TELA INICIAL DA PLATAFORMA
 
 
             <div className={'flex justify-center'}>
-                <div className={`w-11/12 h-48  bg-rosa rounded-r-full mt-20`}>
-                    <div className={'flex items-center justify-around p-4 '}>
-                        <div className={''}>
-                            <div className={'sm:text-center'}>
-                                <p className={'text-gradiente1 font-semibold '}>Auto Avaliação</p>
+                <div className={`w-11/12 h-48 bg-rosa rounded-r-full mt-20`}>
+                    <div className={'flex items-center justify-around p-4'}>
+                        <div className={'2xl:flex gap-20 items-center '}>
+                            <div className={'sm:text-center 2xl:p-8'}>
+                                <p className={'text-gradiente1 font-semibold 2xl:text-2xl sm:text-xl '}>Auto Avaliação</p>
                             </div>
 
                             <div className={''}>
-                                <p className={'text-base sm:p-8 sm:h-32'}>Através de uma página é possível realizar toda a auto avaliação necessária no semestre.</p>
+                                <p className={'text-base sm:p-8 sm:h-32 2xl:text-xl 2xl:mt-9'}>Através de uma página é possível realizar toda a auto avaliação necessária no semestre.</p>
                             </div>
                         </div>
                         <div className={''}>
-                            <img src={animacao2} alt="" className={'w-32 sm:w-28 2xl:w-32'} />
+                            <img src={animacao2} alt="" className={'w-32 sm:w-28 sm:mr-5 xl:w-28 2xl:w-32 '} />
                         </div>
                     </div>
                 </div>
@@ -65,14 +65,14 @@ function Home() {  //TELA INICIAL DA PLATAFORMA
             <div className={'flex justify-center'}>
                 <div className={`w-11/12 h-48  bg-container rounded-l-full mt-20`}>
                     <div className={'flex items-center justify-center mt-4'}>
-                        <p className={'text-dashboard font-semibold'}>Rendimento/Dashboard</p>
+                        <p className={'text-dashboard font-semibold 2xl:text-2xl sm:text-xl'}>Rendimento/Dashboard</p>
                     </div>
                     <div className={'flex items-center justify-evenly'}>
                         <div className={''}>
-                            <img src={animacao3} alt="" className={'w-32 sm:w-24 2xl:w-32'} />
+                            <img src={animacao3} alt="" className={'w-32 sm:w-24 xl:w-28 2xl:w-32'} />
                         </div>
-                        <div className={''}>
-                            <p className={'text-base sm:w-96'}>Todas as notas se transformam em dados, para que seja gerado o rendimento dos aprendizes em formato de gráfico.</p>
+                        <div className={'xl:mr-20'}>
+                            <p className={'text-base sm:w-96 2xl:text-xl'}>Todas as notas se transformam em dados, para que seja gerado o rendimento dos aprendizes em formato de gráfico.</p>
                         </div>
                     </div>
                 </div>
@@ -84,14 +84,14 @@ function Home() {  //TELA INICIAL DA PLATAFORMA
                     <div className={'flex items-center justify-around p-4 sm:p-8'}>
                         <div>
                             <div className={'sm:items-center'}>
-                                <p className={'text-gradiente1 font-semibold sm:p-5'}>Busca de dados</p>
+                                <p className={'text-gradiente1 font-semibold 2xl:text-2xl sm:p-5 sm:text-center sm:text-xl'}>Busca de dados</p>
                             </div>
                             <div className={''}>
-                                <p className={'text-base'}>A plataforma recolhe as notas do SENAI de todos os aprendizes.</p>
+                                <p className={'text-base 2xl:text-xl'}>A plataforma recolhe as notas do SENAI de todos os aprendizes.</p>
                             </div>
                         </div>
                         <div className={''}>
-                            <img src={animacao4} alt="" className={'w-16 sm:w-12 2xl:w-16'} />
+                            <img src={animacao4} alt="" className={'w-16 sm:w-12 sm:mr-8 xl:w-14 2xl:w-16'} />
                         </div>
                     </div>
                 </div>
